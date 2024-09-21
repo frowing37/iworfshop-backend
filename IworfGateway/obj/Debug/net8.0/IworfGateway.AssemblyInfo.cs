159330efd0f56d67e4d8bf2da5b10f020f39c93f
frowing37 <froward37@gmail.com>
2024-09-22 00:36:08 +0300

    initial #3

--- IworfGateway/obj/Debug/net8.0/IworfGateway.AssemblyInfo.cs
+++ IworfGateway/obj/Debug/net8.0/IworfGateway.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApi")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("IworfGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e1557e7e9a8317e31967c12cce3db5ee76347d")]
-[assembly: System.Reflection.AssemblyProductAttribute("IdentityApi")]
-[assembly: System.Reflection.AssemblyTitleAttribute("IdentityApi")]
+[assembly: System.Reflection.AssemblyProductAttribute("IworfGateway")]
+[assembly: System.Reflection.AssemblyTitleAttribute("IworfGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
